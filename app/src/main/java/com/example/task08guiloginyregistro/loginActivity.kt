67b5651f429ed5036572a94cc0de9741be1e33ac
MarcoGarciaa <margar306@gmail.com>
2024-01-16 13:29:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/task08guiloginyregistro/loginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.task08guiloginyregistro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class loginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+
+}
